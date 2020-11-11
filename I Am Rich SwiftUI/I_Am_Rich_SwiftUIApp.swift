6e022f1b0daa7aa6430cef1a2022b500120d3190
Juan Diego Ocampo <jd.ocampom0117@icloud.com>
2020-11-11 10:00:55 -0500

    Initial Commit

---
+++ I Am Rich SwiftUI/I_Am_Rich_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_SwiftUIApp.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Juan Diego Ocampo on 11/11/20.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
